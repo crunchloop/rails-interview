82acfd24c0d4d4e7e6c57db237bd4f74171e97eb
htopo <49622428+htopo@users.noreply.github.com>
2024-05-19 22:15:40 -0300

    Add TodoItem validations

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -4,4 +4,8 @@ class TodoItem < ApplicationRecord
   belongs_to :todo_list
 
   validates :todo_list_id, presence: true
+  validates :name, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :todo_list_id, presence: true
+  validates :completed, inclusion: { in: [true, false] }
+  validates :description, length: { maximum: 500 }, allow_blank: true
 end
--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe TodoItem, type: :model do
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:todo_list_id) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_least(3).is_at_most(100) }
+    it { is_expected.to validate_inclusion_of(:completed).in_array([true, false]) }
+    it { is_expected.to validate_length_of(:description).is_at_most(500) }
   end
 end
